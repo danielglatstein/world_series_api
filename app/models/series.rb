12c78e258bb9ceb5fda78e14a81c61d4c87533e4
danielglatstein <danielglatstein@gmail.com>
2015-11-30 19:06:26 -0500

    create played against method

--- app/models/series.rb
+++ app/models/series.rb
@@ -1,3 +1,9 @@
 class Series < ActiveRecord::Base
   has_many :teams
+  def winner
+    self.winner_id
+  end
+  def loser
+    self.loser_id
+  end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,17 @@
 class Team < ActiveRecord::Base
   has_many :series
+  
+  def played_against
+    opponents = []
+    all_series = Series.all
+    all_series.each_with_object(opponents) do |series, opponents|
+      if series.winner_id == self.id
+        opponents << series.loser_id
+      elsif series.loser_id == self.id
+        opponents << series.winner_id
+      end
+    end
+    opponents
+  end
+
 end
